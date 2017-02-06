89d73ae694203cbd2376428ade8a08545d759e0f
Brett Mordas <brett.mordas@gmail.com>
2017-02-06 12:53:21 -0500

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// config/db.js
+
+module.exports = {
+	url : 'C:\Program Files\dev\MongoDB\Server\3.4\data\db'
+}
---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/MainCtrl.js
+
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+	$scope.tagline = 'To the moon and back!';
+	console.log('Dude, you made it to the MainController.');
+});
